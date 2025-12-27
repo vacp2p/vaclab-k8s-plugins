54e5d58dd09730f8f920df39e89448e3071dd895
mamoutou-diarra <mamoutou.diarram@gmail.com>
2025-12-27 13:09:41 +0000

    rebase operator repo

--- bandwidth-operator/internal/controller/setup_bandwidth.go
+++ bandwidth-operator/internal/controller/setup_bandwidth.go
@@ -6,7 +6,7 @@ import (
 	"sync"
 	"time"
 
-	networkingv1 "github.com/vacp2p/vaclab-k8s-plugins/api/v1"
+	networkingv1 "github.com/vacp2p/vaclab-k8s-plugins/bandwidth-operator/api/v1"
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
